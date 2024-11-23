ba8f83206b6a84643abf17ba8ce6451d859530d6
googer_ <googer@mindurka.fun>
2024-11-23 12:58:10 +0700

    inversion changes

--- lua/autorun/sh_paranatural_inversion.lua
+++ lua/autorun/sh_paranatural_inversion.lua
@@ -6,6 +6,7 @@ end
 
 hook.Add("EntityTakeDamage", "paranatural_inversion", function(target, dmginfo)
     if (dmginfo:GetAttacker().paranatural_iv_inverted or false) == (target.paranatural_iv_inverted or false) then return end
+    if dmginfo:IsDamageType(DMG_DISSOLVE + DMG_FALL + DMG_DROWN) then return end
     local dmg = dmginfo:GetDamage()
     dmginfo:ScaleDamage(0)
     target:SetHealth(math.min(target:GetMaxHealth(), target:Health() + dmg))
@@ -18,6 +19,23 @@ end)
 -- TODO: way to damage uninverted players
 -- TODO: invert chat messages
 -- TODO: invert sounds?
+
+local function do_check_antimatdissolve(ent, data)
+    local ent1, ent2 = ent, data.HitEntity
+    if ent1 == game.GetWorld() or ent2 == game.GetWorld() then return end
+    if not IsValid(ent1) or not IsValid(ent2) then return end
+
+    if ent1:GetNWBool("paranatural_iv_inverted", false) == ent2:GetNWBool("paranatural_iv_inverted", false) then return end
+
+    local d_ent1, d_ent2 = DamageInfo(), DamageInfo()
+	d_ent1:SetDamage(ent2:Health()) d_ent2:SetDamage(ent1:Health())
+	d_ent1:SetAttacker(ent1) d_ent2:SetAttacker(ent2)
+	d_ent1:SetDamageType(DMG_DISSOLVE) d_ent2:SetDamageType(DMG_DISSOLVE)
+
+	ent2:TakeDamageInfo(d_ent1)
+	ent1:TakeDamageInfo(d_ent2)
+end
+
 hook.Add("PlayerButtonDown", "paranatural_inversion", function(ply, button)
     if CLIENT then return end
 	if not allowed:GetBool() and not ply:IsAdmin() then return end
@@ -28,16 +46,30 @@ hook.Add("PlayerButtonDown", "paranatural_inversion", function(ply, button)
     ply:SetNWBool("paranatural_iv_inverted", ply.paranatural_iv_inverted)
     if ply.paranatural_iv_inverted then
         ply:ChatPrint("Inverted")
+        ply.paranatural_iv_callback = ply:AddCallback("PhysicsCollide", do_check_antimatdissolve)
 		-- TODO: play inversion sound
     else
         ply:ChatPrint("Normal")
+        ply:RemoveCallback("PhysicsCollide", ply.paranatural_iv_callback)
+        ply.paranatural_iv_callback = nil
 		-- TODO: play uninversion sound
     end
 end)
 
 local need_to_redraw = {}
 local drawing = false
-local invert_tbl = {["$pp_colour_inv"] = 1}
+local invert_tbl = {
+    ["$pp_colour_inv"] = 1,
+	["$pp_colour_addr"] = 0,
+	["$pp_colour_addg"] = 0,
+	["$pp_colour_addb"] = 0,
+	["$pp_colour_brightness"] = 0,
+	["$pp_colour_contrast"] = 1,
+	["$pp_colour_colour"] = 1,
+	["$pp_colour_mulr"] = 0,
+	["$pp_colour_mulg"] = 0,
+	["$pp_colour_mulb"] = 0
+}
 hook.Add("PreRender", "paranatural_inversion", function() need_to_redraw = {} end)
 
 hook.Add("PlayerDeath", "paranatural_inversion", function(ply)
@@ -110,7 +142,7 @@ hook.Add("RenderScreenspaceEffects", "paranatural_inversion", function()
 		draw(ply)
     end
 
-        DrawColorModify(invert_tbl)
+    DrawColorModify(invert_tbl)
 
     render.SetStencilEnable(false)
     for _,ply in pairs(need_to_redraw) do
@@ -120,4 +152,18 @@ hook.Add("RenderScreenspaceEffects", "paranatural_inversion", function()
     cam.End3D()
 
     drawing = false
+end)
+
+local color_white = Color(255, 255, 255, 255)
+hook.Add("OnPlayerChat", "paranatural_inversion", function(ply, strText)
+    -- if ply == LocalPlayer() then return end
+
+    if ply:GetNWBool("paranatural_iv_inverted", false) == LocalPlayer():GetNWBool("paranatural_iv_inverted", false) then return end
+
+    local team_color = hook.Run("GetTeamColor", ply)
+    team_color = Color(255-team_color.r, 255-team_color.g, 255-team_color.b, team_color.a)
+
+    chat.AddText(team_color, string.reverse(ply:Name()), color_white, ": ", string.reverse(strText))
+
+    return true
 end)
